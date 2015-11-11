17934367888a3565369123970180817b639e8373
Damian Jardim <dljardim@outlook.com>
2015-11-11 09:33:16 -0500

    initial commit

---
+++ bash/echo.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# the first line says to use the bash shell
+# it is called a sha-bang
+
+# This is a comment
+
+# to make scripts executable
+# chmod 755 echo.sh
+
+# to execute
+# ./echo.sh
+
+# run echo with 1 parameter
+echo "Hello,        World"
+
+# run echo with 2 parameters
+# echo Hello World
---
+++ bash/echo2.sh
@@ -0,0 +1,5 @@
+# this bash script does not contain a sha-bang
+# to execute this script:
+# sh echo2.sh;
+
+echo "echo2.sh is being run";
